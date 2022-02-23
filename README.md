1d3f92a81c6cfd26cfe754a93b08c5027f822ae7
MohitRaw <mohitrawat.221@gmail.com>
2022-02-23 15:10:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Clone using https then use command git clone https://............. .
+After that go to that directory and use command npm Install.
+You can run it on localhost using npm run dev.
+ 
---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "cdn.freelogovectors.net",
+      "a0.muscache.com",
+      "images.contentstack.io",
+      "pbs.twimg.com",
+      "links.papareact.com"
+    ],
+  },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoibW9oaXQyMjEiLCJhIjoiY2t5ZTEzd2R5MDlyazMwcXBrbWozeHN4MyJ9.Aa9FEgmvRJxZDhx7WimKig',
+  },
+};
